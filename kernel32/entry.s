72d2359d53abeeb26f3683d45e82374ff66bc22f
Jyang772 <Jyang772@gmail.com>
2014-03-24 00:32:29 -0700

    A20 activation

--- kernel32/entry.s
+++ kernel32/entry.s
@@ -8,10 +8,47 @@ START:
 
 
 ;========= A20 GATE ========
+				;Enable A20 using BIOS
+	mov ax, 0x2401
+	int 0x15
 
-;	;mov ax, 0x2401
-;	;int 0x15
+	jc A20GATEERROR		;If fail. Try to enable A20 via System Port A
+	jmp A20GATESUCCESS
 
+
+A20GATEERROR:
+	cli
+	
+	lgdt [GDTR]			;Special CPU instruction to load GDT
+		
+	mov eax, 0x4000003B		; PG=0, CD=1, NW=0, AM=0, WP=0, NE=1, ET=1, TS=1, EM=0, MP=1, PE=1 
+	mov cr0, eax
+
+	jmp dword 0x18:(PROTECTEDMODE - $$ + 0x10000)
+
+
+[BITS 32]
+PROTECTEDMODE:
+	mov ax, 0x10			;Set up stack!
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	
+
+	mov ss, ax
+	mov esp, 0xffff
+	mov ebp, 0xffff
+	
+	push (MODESWITCHMESSAGE - $$ + 0x10000)
+	push 3
+	push 0
+	call PRINTMESSAGE
+	add 	esp, 12	
+
+
+
+		
 
 
 	mov ah, 2
